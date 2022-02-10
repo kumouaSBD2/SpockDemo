17349360eb5f860bf2d44a3441a219d0a617bc33
kumouaSBD2 <ku.moua@sbd2.com>
2022-02-10 13:21:41 -0500

    Initial Commit - Spock Exercise

---
+++ src/test/java/com/example/SpockDemo/SpockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpockDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpockDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
